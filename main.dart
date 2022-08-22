67a54d0377dd537091f57d5cd02f7b51aea1bd62
belone <belone00@gmail.com>
2022-08-22 12:33:10 -0300

    Flutter Imc calculator added

---
+++ main.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'IMC Calculator',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'IMC Calculator'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  double imc = 0;
+  double altura = 0;
+  double peso = 0;
+
+  void IMC(double a, double p) {
+    setState(() {
+
+      imc = p / (a*a);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text(widget.title),
+      ),
+      body: Center(
+
+        child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+
+             TextField(
+              onChanged: (text){
+                peso = double.parse(text);
+            },
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Peso',
+              ),
+            ),
+
+             TextField(
+               onChanged: (text){
+                 altura = double.parse(text);
+               },
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Altura',
+              ),
+            ),
+
+            RaisedButton(
+                child: const Text('Calcular IMc'),
+                elevation: 4.0,
+              onPressed: (){
+                  IMC(altura,peso);
+              },
+            ),
+
+              Text('IMC: '),
+              Text(imc.toString()),
+          ],
+        ),
+      ),
+    );
+  }
+}
